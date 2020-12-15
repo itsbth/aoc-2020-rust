525ddb8925a700e3b893b97149545097aef4862d
Bjørn Tore Håvie <bjoern.tore.haavie@pexip.com>
2020-12-15 13:32:16 +0100

    day15: bad solution is bad

---
+++ src/day15.rs
@@ -0,0 +1,48 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::collections::HashMap;
+use std::str::FromStr;
+
+type Input = Vec<i64>;
+
+#[aoc_generator(day15)]
+pub fn generator(nums: &str) -> Input {
+    nums.split(',').map(|n| i64::from_str(n).unwrap()).collect()
+}
+
+#[aoc(day15, part1)]
+pub fn part1(code: &Input) -> i64 {
+    let mut seen = HashMap::<i64, usize>::new();
+    let mut last = *code.iter().last().unwrap();
+    for (idx, n) in code[..code.len() - 1].iter().enumerate() {
+        seen.insert(*n, idx);
+    }
+    for i in code.len()..2020 {
+        let cur = if let Some(r) = seen.get(&last) {
+            i - r - 1
+        } else {
+            0
+        };
+        seen.insert(last as i64, i - 1);
+        last = cur as i64;
+    }
+    last
+}
+
+#[aoc(day15, part2)]
+pub fn part2(code: &Input) -> i64 {
+    let mut seen = HashMap::<i64, usize>::new();
+    let mut last = *code.iter().last().unwrap();
+    for (idx, n) in code[..code.len() - 1].iter().enumerate() {
+        seen.insert(*n, idx);
+    }
+    for i in code.len()..30000000 {
+        let cur = if let Some(r) = seen.get(&last) {
+            i - r - 1
+        } else {
+            0
+        };
+        seen.insert(last as i64, i - 1);
+        last = cur as i64;
+    }
+    last
+}
